3974cce9ead55201a0e679a7b0e0f91bc6d0901d
MynnTapp <Desmynnmjc@gmail.com>
2024-10-15 10:57:56 -0500

    finished Navigation

--- frontend/src/components/Navigation/ProfileButton.jsx
+++ frontend/src/components/Navigation/ProfileButton.jsx
@@ -1,1 +1,33 @@
 import { FaModx } from "react-icons/fa6";
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import * as sessionActions from "../../store/session";
+
+function ProfileButton({ user }) {
+  const dispatch = useDispatch();
+
+  const logout = (e) => {
+    e.preventDefault();
+    dispatch(sessionActions.logout());
+  };
+
+  return (
+    <>
+      <button>
+        <FaModx />
+      </button>
+      <ul className="profile-dropdown">
+        <li>{user.username}</li>
+        <li>
+          {user.firstName} {user.lastName}
+        </li>
+        <li>{user.email}</li>
+        <li>
+          <button onClick={logout}>Log Out</button>
+        </li>
+      </ul>
+    </>
+  );
+}
+
+export default ProfileButton;
