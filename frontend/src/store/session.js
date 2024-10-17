0dae890d897b8f9de06523cad0a1b85140f5b7f2
MynnTapp <Desmynnmjc@gmail.com>
2024-10-16 20:30:32 -0500

    fixed page refresh on signup

--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -1,3 +1,88 @@
+// // frontend/src/store/session.js
+
+// import { csrfFetch } from "./csrf";
+
+// const SET_USER = "session/setUser";
+// const REMOVE_USER = "session/removeUser";
+
+// const setUser = (user) => {
+//   return {
+//     type: SET_USER,
+//     payload: user,
+//   };
+// };
+
+// const removeUser = () => {
+//   return {
+//     type: REMOVE_USER,
+//   };
+// };
+
+// export const login = (user) => async (dispatch) => {
+//   const { credential, password } = user;
+//   const response = await csrfFetch("/api/session", {
+//     method: "POST",
+//     body: JSON.stringify({
+//       credential,
+//       password,
+//     }),
+//   });
+//   const data = await response.json();
+//   dispatch(setUser(data.user));
+
+//   return response;
+// };
+
+// const initialState = { user: null };
+
+// const sessionReducer = (state = initialState, action) => {
+//   switch (action.type) {
+//     case SET_USER:
+//       return { ...state, user: action.payload };
+//     case REMOVE_USER:
+//       return { ...state, user: null };
+//     default:
+//       return state;
+//   }
+// };
+
+// export const restoreUser = () => async (dispatch) => {
+//   const response = await csrfFetch("/api/session");
+//   const data = await response.json();
+//   dispatch(setUser(data.user));
+//   return response;
+// };
+
+// export const signup = (user) => async (dispatch) => {
+//   const { username, firstName, lastName, email, password } = user;
+//   const response = await csrfFetch("/api/users", {
+//     method: "POST",
+//     body: JSON.stringify({
+//       username,
+//       firstName,
+//       lastName,
+//       email,
+//       password,
+//     }),
+//   });
+//   const data = await response.json();
+//   dispatch(setUser(data.user));
+//   return data;
+// };
+
+// // frontend/src/store/session.js
+
+// // ...
+// export const logout = () => async (dispatch) => {
+//   const response = await csrfFetch("/api/session", {
+//     method: "DELETE",
+//   });
+//   dispatch(removeUser());
+//   return response;
+// };
+
+// export default sessionReducer;
+
 // frontend/src/store/session.js
 
 // import { csrfFetch } from "./csrf";
@@ -139,6 +224,23 @@ export const restoreUser = () => async (dispatch) => {
 };
 
 // Signup action
+// export const signup = (user) => async (dispatch) => {
+//   const { username, firstName, lastName, email, password } = user;
+//   const response = await csrfFetch("/api/users", {
+//     method: "POST",
+//     body: JSON.stringify({
+//       username,
+//       firstName,
+//       lastName,
+//       email,
+//       password,
+//     }),
+//   });
+//   const data = await response.json();
+//   dispatch(setUser(data.user));
+//   return data;
+// };
+
 export const signup = (user) => async (dispatch) => {
   const { username, firstName, lastName, email, password } = user;
   const response = await csrfFetch("/api/users", {
@@ -151,9 +253,19 @@ export const signup = (user) => async (dispatch) => {
       password,
     }),
   });
+
   const data = await response.json();
-  dispatch(setUser(data.user));
-  return data;
+  console.log("Response data from server:", data); // Add this log
+
+  if (data.token) {
+    console.log("Received token:", data.token); // Add this log
+    localStorage.setItem("token", data.token);
+    dispatch(setUser(data.user));
+  } else {
+    console.error("Token is missing in the response");
+  }
+
+  return response;
 };
 
 // Logout action
